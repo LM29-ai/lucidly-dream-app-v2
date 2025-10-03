071cb4ac89329f98c90c1b306dc8ebd7104cf8a6
LM29-ai <hilliard784@gmail.com>
2025-10-03 17:30:59 -0500

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -4,7 +4,8 @@ export default {
     name: "Lucidly",
     slug: "lucidly",
     version: "1.0.0",
-
+extra: {
+      eas: { projectId: "a38e5048-5154-4fe9-ac8c-a58e3cd72b61" }
     // Android package (required for EAS GitHub builds)
     android: {
       package: "com.lm29ai.lucidly", // <-- change if you use a different app id
